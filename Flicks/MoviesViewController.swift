dbaea9467782de388236221eb23507906c78067f
Nishant nishanko <nishantnishanko@Nishants-MacBook-Pro.local>
2017-03-31 23:27:11 -0700

    firstcommit

---
+++ Flicks/MoviesViewController.swift
@@ -0,0 +1,99 @@
+//
+//  MoviesViewController.swift
+//  Flicks
+//
+//  Created by Nishant nishanko on 3/30/17.
+//  Copyright © 2017 Nishant nishanko. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    var movies: [NSDictionary]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        
+        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+ 
+                        //let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+//                        print ("response: \(responseDictionary)")
+                        
+                        self.movies = responseDictionary["results"] as! [NSDictionary]
+                        self.tableView.reloadData()
+                        
+                     }
+                }
+        });
+        task.resume()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let movies = movies{
+            return movies.count
+            
+        }else{
+            return 0
+        }
+    
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        let baseurl = "https://image.tmdb.org/t/p/w500"
+        if let posterPath = movie["poster_path"] as? String{
+            let imageUrl = URL(string: baseurl+posterPath)
+            cell.posterView.setImageWith(imageUrl!)
+
+        }
+        
+        
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        return cell
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)
+        let movie = movies?[(indexPath?.row)!]
+        var destinationViewController = segue.destination as! DetailViewController
+        destinationViewController.movie = movie
+ 
+    }
+    
+}
